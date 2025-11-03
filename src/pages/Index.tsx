590bcc26188d6badea4294dd10fe1a022edba69a
DENBOBA Abdulkadir Gobena <abdulkadirden96@gmail.com>
2025-11-03 11:42:48 +0200

    bug in project name

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,7 +22,7 @@ const Index = () => {
               
               <h1 className="text-5xl md:text-7xl font-bold tracking-tight animate-fade-in">
                 <span className="bg-gradient-to-r from-primary via-accent to-primary bg-clip-text text-transparent">
-                  Examertric
+                  Exametric
                 </span>
               </h1>
               
